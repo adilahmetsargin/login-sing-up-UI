7b401eb1c05e25827df1cbfacc1a2a9a9cd361ad
Adil Ahmet Sargın <ahmet.sargin@westerops.com>
2021-11-08 21:50:07 +0300

    first commit

---
+++ lib/view/signup_page.dart
@@ -0,0 +1,205 @@
+// ignore_for_file: prefer_const_constructors, avoid_unnecessary_containers, unnecessary_string_escapes
+// ignore_for_file: prefer_const_literals_to_create_immutables
+
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class SignUpPage extends StatelessWidget {
+  const SignUpPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    List images = [
+      "g.png",
+      "t.png",
+      "f.png",
+    ];
+
+    double w = MediaQuery.of(context).size.width;
+    double h = MediaQuery.of(context).size.height;
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Column(
+        children: [
+          Container(
+            width: w,
+            height: h * 0.3,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/signup.png'),
+                fit: BoxFit.cover,
+              ),
+            ),
+            child: Column(
+              children: [
+                SizedBox(height: h * 0.16),
+                CircleAvatar(
+                    backgroundColor: Colors.white70,
+                    radius: 60,
+                    backgroundImage: AssetImage('assets/profile.png'))
+              ],
+            ),
+          ),
+          Container(
+            width: w,
+            margin: EdgeInsets.only(left: 20, right: 20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Hello',
+                  style: TextStyle(
+                    fontSize: 70,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  'Sign into your account',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.grey,
+                  ),
+                ),
+                SizedBox(
+                  height: 50,
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(30),
+                      boxShadow: [
+                        BoxShadow(
+                          blurRadius: 10,
+                          spreadRadius: 7,
+                          offset: Offset(1, 1),
+                          color: Colors.grey.withOpacity(0.2),
+                        ),
+                      ]),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: "Email",
+                      prefixIcon:
+                          Icon(Icons.email, color: Colors.deepOrangeAccent),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(30),
+                        borderSide: BorderSide(
+                          color: Colors.white,
+                          width: 1.0,
+                        ),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Colors.white,
+                          width: 1.0,
+                        ),
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(30),
+                      boxShadow: [
+                        BoxShadow(
+                          blurRadius: 10,
+                          spreadRadius: 7,
+                          offset: Offset(1, 1),
+                          color: Colors.grey.withOpacity(0.2),
+                        ),
+                      ]),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: "Password",
+                      prefixIcon:
+                          Icon(Icons.lock, color: Colors.deepOrangeAccent),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(30),
+                        borderSide: BorderSide(
+                          color: Colors.white,
+                          width: 1.0,
+                        ),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Colors.white,
+                          width: 1.0,
+                        ),
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(height: 70),
+          Container(
+            width: w * 0.5,
+            height: h * 0.08,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(30),
+              image: DecorationImage(
+                image: AssetImage('assets/loginbtn.png'),
+                fit: BoxFit.cover,
+              ),
+            ),
+            child: Center(
+              child: Text(
+                'Sign up',
+                style: TextStyle(
+                    fontSize: 26,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white),
+              ),
+            ),
+          ),
+          SizedBox(height: 30),
+          RichText(
+            text: TextSpan(
+                recognizer: TapGestureRecognizer()..onTap = () => Get.back(),
+                text: "Have an account?",
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.grey[500],
+                )),
+          ),
+          SizedBox(height: 30),
+          RichText(
+            text: TextSpan(
+              text: "Sign up using one of the following methods",
+              style: TextStyle(color: Colors.black, fontSize: 14),
+            ),
+          ),
+          Wrap(
+            children: List<Widget>.generate(
+              3,
+              (index) {
+                return Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: CircleAvatar(
+                    radius: 15,
+                    backgroundColor: Colors.grey[500],
+                    child: CircleAvatar(
+                      radius: 15,
+                      backgroundImage: AssetImage("assets/" + images[index]),
+                    ),
+                  ),
+                );
+              },
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/view/welcome_page.dart
@@ -0,0 +1,86 @@
+// ignore_for_file: prefer_const_constructors, avoid_unnecessary_containers, unnecessary_string_escapes
+// ignore_for_file: prefer_const_literals_to_create_immutables
+
+import 'package:flutter/material.dart';
+
+class WelcomePage extends StatelessWidget {
+  const WelcomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    double w = MediaQuery.of(context).size.width;
+    double h = MediaQuery.of(context).size.height;
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Column(
+        children: [
+          Container(
+            width: w,
+            height: h * 0.3,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/signup.png'),
+                fit: BoxFit.cover,
+              ),
+            ),
+            child: Column(
+              children: [
+                SizedBox(height: h * 0.16),
+                CircleAvatar(
+                    backgroundColor: Colors.white70,
+                    radius: 60,
+                    backgroundImage: AssetImage('assets/profile.png'))
+              ],
+            ),
+          ),
+          SizedBox(height: 70),
+          Container(
+            width: w,
+            margin: EdgeInsets.only(left: 20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Welcome",
+                  style: TextStyle(
+                    fontSize: 36,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black54,
+                  ),
+                ),
+                Text(
+                  "a@a.com",
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.grey[500],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(height: 200),
+          Container(
+            width: w * 0.5,
+            height: h * 0.08,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(30),
+              image: DecorationImage(
+                image: AssetImage('assets/loginbtn.png'),
+                fit: BoxFit.cover,
+              ),
+            ),
+            child: Center(
+              child: Text(
+                'Sign out',
+                style: TextStyle(
+                    fontSize: 26,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
